46afa34c3f6093a1965401ad8b3b2901d6096ef9
David Laehnemann <david.laehnemann@hhu.de>
2025-06-23 11:42:54 +0200

    feat: add cellranger install from CELLRANGER_TARBALL envvar

---
+++ workflow/envs/cellranger.post-deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# set all the necessary conda paths and
+# ensure they exist
+CONDA_BIN="${CONDA_PREFIX}/bin/"
+
+CONDA_LIB="${CONDA_PREFIX}/lib/"
+mkdir -p ${CONDA_LIB}
+
+# install cellranger
+cd ${CONDA_LIB}
+tar xzf ${CELLRANGER_TARBALL}
+CELLRANGER_DIR=$( ls -d cellranger* )
+""
+
+ln -S ${CONDA_LIB}/${CELLRANGER_DIR}/cellranger ${CONDA_BIN}/cellranger
