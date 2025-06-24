f2a6a9ce4c12f8857ad2a231b54a094b57c29c87
David Laehnemann <david.laehnemann@hhu.de>
2025-06-24 14:17:18 +0200

    fix: use correct flag for symbolic link

--- workflow/envs/cellranger.post-deploy.sh
+++ workflow/envs/cellranger.post-deploy.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 # set all the necessary conda paths and
 # ensure they exist
-CONDA_BIN="${CONDA_PREFIX}/bin/"
+CONDA_BIN="${CONDA_PREFIX}/bin"
 
-CONDA_LIB="${CONDA_PREFIX}/lib/"
+CONDA_LIB="${CONDA_PREFIX}/lib"
 mkdir -p ${CONDA_LIB}
 
 # install cellranger
@@ -13,7 +13,7 @@ cd ${CONDA_LIB}
 tar xzf ${CELLRANGER_TARBALL}
 CELLRANGER_DIR=$( ls -d cellranger* )
 
-ln -S ${CONDA_LIB}/${CELLRANGER_DIR}/cellranger ${CONDA_BIN}/cellranger
+ln -s ${CONDA_LIB}/${CELLRANGER_DIR}/cellranger ${CONDA_BIN}/cellranger
 
 # check that the cellranger executable is available and works
 cellranger testrun --id=tiny
