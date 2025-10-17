39fcbf665c9b0f673e38fce03a817115241ee586
dlaehnemann <1379875+dlaehnemann@users.noreply.github.com>
2025-10-17 11:58:28 +0200

    feat: initial script version for multi config csv creation (should contain all the necessary logic, not yet tested)

--- workflow/scripts/create_cellranger_multi_config_csv.R
+++ workflow/scripts/create_cellranger_multi_config_csv.R
@@ -6,44 +6,132 @@ rlang::global_entrace()
 
 library(tidyverse)
 
-
 libraries_table <- read_tsv(snakemake@input[["sample_sheet"]]) |>
-  # add NA column if the sample sheet does not list library_type
-  bind_rows(
-    tibble(
-      library_type = character()
-    )
-  ) |>
   filter(
     sample == snakemake@wildcards[["sample"]]
-  ) |>
-  # provide useful default, if library_type is not specified
-  mutate(
-    library_type = replace_na(library_type, "Gene Expression")
   ) |>
   add_column(
     fastqs = snakemake@params[["fastqs_dir"]]
   ) |>
-  select(
-    fastqs,
-    sample,
-    library_type
+  rename(
+    fastq_id = sample
   ) |>
   # we might have multiple lanes per sample in the main sample
   # sheet, but only need one entry per sample here
-  distinct()
+  summarize(
+    lanes = str_flatten(
+      lane_number,
+      collapse = "|"
+    ),
+    .by = any_of(
+      c(
+        "fastq_id",
+        "fastqs",
+        "feature_types",
+        "physical_library_id",
+        "subsample_rate",
+        "chemistry"
+      )
+    )
+  ) |>
+  select(
+    any_of(
+      c(
+        "fastq_id",
+        "fastqs",
+        "feature_types",
+        "lanes",
+        "physical_library_id",
+        "subsample_rate",
+        "chemistry"
+      )
+    )
+  )
 
-# Only start writing anything after we have done all the parsing.
+specified_feature_types <- libraries_table |> pull(feature_types)
+
+# Only start writing anything after we have done the libraries parsing. 
+# Otherwise, we need to debug the sample sheet, anyways.
 
 write_lines(
-  "[gene-expression]",
+  "",
   file = snakemake@output[["multi_config_csv"]],
-  append = FALSE # ensure that the file gets overwritten with every script execution
+  sep = "", # ensure that the file gets overwritten with every script execution
+  append = FALSE
 )
 
+parse_and_write_section_if_required <- function(
+  feature_types,
+  section_heading
+) {
+  if (any(feature_types) %in% specified_feature_types) {
+    section_table <- enframe(
+      snakemake@params[["multi_config_csv_sections"]][[section_heading]]
+    )
+    write_lines(
+      c("", str_c("[", section_heading, "]"))
+      file = snakemake@output[["multi_config_csv"]],
+      append = TRUE
+    )
+    write_csv(
+      section_table,
+      file = snakemake@output[["multi_config_csv"]],
+      col_names = FALSE,
+      append = TRUE
+    )
+  }
+}
+
+parse_and_write_section_if_required(
+  c(
+    "Gene Expression"
+  ),
+  "gene-expression"
+)
+
+parse_and_write_section_if_required(
+  c(
+    "VDJ", "VDJ-T", "VDJ-T-GD", "VDJ-B"
+  ),
+  "vdj"
+)
+
+parse_and_write_section_if_required(
+  c(
+    "Antibody Capture", "Antigen Capture", "CRISPR Guide Capture"
+  ),
+  "feature"
+)
+
+# parsing for antigen-specificity section is different, so we do it without the helper
+
+if ("Antigen Capture" %in% feature_types) {
+  antigen_specificity_table <- enframe(
+    snakemake@params[["multi_config_csv_sections"]][["antigen-specificity"]][[
+      "control_ids"
+    ]],
+    name = "control_id",
+    value = "mhc_allele"
+  )
+  if (all(antigen_specificity_table |> pull(mhc_allele) == "")) {
+    antigen_specificity_table <- antigen_specificity_table |> select(-mhc_allele)
+  }
+  write_lines(
+    c("", str_c("[antigen-specificity]"))
+    file = snakemake@output[["multi_config_csv"]],
+    append = TRUE
+  )
+  write_csv(
+    antigen_specificity_table,
+    file = snakemake@output[["multi_config_csv"]],
+    append = TRUE
+  )
+}
+
+# parsing for the libraries section is different, so we write without the helper
 
 write_lines(
-  "[libraries]",
+  c("", "[libraries]"),
   file = snakemake@output[["multi_config_csv"]],
   append = TRUE
 )
