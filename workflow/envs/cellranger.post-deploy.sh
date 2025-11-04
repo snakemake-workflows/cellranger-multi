2bed6677a30330230aafa633efd17772583f8e44
dlaehnemann <1379875+dlaehnemann@users.noreply.github.com>
2025-11-04 14:35:50 +0100

    fix: disable telemetry during cellranger installation

--- workflow/envs/cellranger.post-deploy.sh
+++ workflow/envs/cellranger.post-deploy.sh
@@ -16,5 +16,10 @@ CELLRANGER_DIR=$( ls -d cellranger* )
 
 ln -s ${CONDA_LIB}/${CELLRANGER_DIR}/cellranger ${CONDA_BIN}/cellranger
 
+# disable telemetry, as this seems to hang the testrun for a very long time
+cellranger telemetry disable upload
+cellranger telemetry disable update
+cellranger telemetry disable all
+
 # check that the cellranger executable is available and works
 cellranger testrun --id=tiny
