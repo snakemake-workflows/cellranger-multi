dd35c504b9bb69e2a46d8f5b534c90f00f7e524c
dlaehnemann <1379875+dlaehnemann@users.noreply.github.com>
2025-11-04 20:12:04 +0100

    fix: bugs in bash script for file existence check and touch, apply suggestions by coderabbitai

--- workflow/scripts/check_cellranger_outputs.sh
+++ workflow/scripts/check_cellranger_outputs.sh
@@ -2,15 +2,15 @@
 
 ( # ensure all of the
   any_missing=0
-  for f in ${snakemake_output}; do
+  for f in "${snakemake_output[@]}"; do
     if [ ! -e "$f" ]; then
       any_missing=1
-      echo \"Missing expected cellranger multi output: $f\" >&2
+      echo "Missing expected cellranger multi output: $f" >&2
     fi
   done;
   if [ "$any_missing" -eq 1 ]; then
     exit 1
   fi
   # if all exist, ensure timestamp of output is newer than the input
-  touch ${snakemake_output};
+  touch "${snakemake_output[@]}";
 ) >${snakemake_log[0]} 2>&1
