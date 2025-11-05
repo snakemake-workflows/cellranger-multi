70481c46b4445d593baba9b3f530ac6a658ad766
dlaehnemann <1379875+dlaehnemann@users.noreply.github.com>
2025-11-05 14:28:56 +0100

    debug: debugging output

--- workflow/scripts/check_cellranger_outputs.sh
+++ workflow/scripts/check_cellranger_outputs.sh
@@ -2,7 +2,9 @@
 
 ( # ensure all of the
   any_missing=0
+  echo "Will check files: ${snakemake_output[@]}"
   for f in "${snakemake_output[@]}"; do
+    echo "Now checking file: $f"
     if [ ! -e "$f" ]; then
       any_missing=1
       echo "Missing expected cellranger multi output: $f" >&2
