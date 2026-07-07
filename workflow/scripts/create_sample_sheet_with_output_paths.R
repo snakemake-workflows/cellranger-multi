2533128e8838a1b4871195f01efe9ff575895fc4
David Laehnemann <1379875+dlaehnemann@users.noreply.github.com>
2026-07-07 21:36:10 +0200

    fix: remove debugging code, remove offending trailing comma

--- workflow/scripts/create_sample_sheet_with_output_paths.R
+++ workflow/scripts/create_sample_sheet_with_output_paths.R
@@ -7,15 +7,11 @@ rlang::global_entrace()
 
 library(tidyverse)
 
-save.image(file="samples.RData")
-
 samples <- enframe(
     unlist(snakemake@input),
     name = NULL,
-    value = "per_sample_counts_path",
-  )
-print(samples)
-# |>
+    value = "per_sample_counts_path"
+  ) |>
   filter(
     str_detect(
       per_sample_counts_path,
@@ -34,8 +30,8 @@ print(samples)
     cols_remove = FALSE
   ) |>
   mutate(
-    per_sample_counts_path = str_remove(per_sample_counts_path, fixed("/sample_filtered_feature_bc_matrix/matrix.mtx.gz")),
-    per_sample_counts_path = str_c(c("../", per_sample_counts_path))
+    per_sample_counts_path = str_remove(per_sample_counts_path, fixed("sample_filtered_feature_bc_matrix/matrix.mtx.gz")),
+    per_sample_counts_path = str_c("../", per_sample_counts_path)
   ) |>
   select(
     sample_id,
